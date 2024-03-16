5f337bac9e146d8b5dc3b4800d73928f914b87e0
oskar2115 <oskarpiotrkraw@gmail.com>
2024-03-16 17:05:32 +0100

    routers fix

---
+++ dist/assets/AboutView-BydsurAJ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as o,o as t}from"./index-BJ-6lHab.js";const c={};function r(n,s){return t(),o("h1",null,"About")}const f=e(c,[["render",r]]);export{f as default};
